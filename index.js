ca84777170948571f8d028fe3c5ea8c42d7b769f
Sahil Mahamulkar <sahilmahamulkar2003@gmail.com>
2024-07-22 13:15:47 +0530

    new

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
+require("dotenv").config()
 const express = require("express");
-const port = 3000;
+// const port = 3000;
 const app = express();
+const port = process.env.PORT;
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/node_modules/bootstrap"));
 
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
+    res.render(__dirname + "/views/index");
 });
 
-app.get("/",(req , res) => {
-    res.sendFile(__dirname + "/views/about.html")
-})
+
+
+app.get("/about",(req , res) => {
+    res.render(__dirname + "/views/about");
+});
+
+app.get("/contact",(req , res) => {
+    res.render(__dirname + "/views/contact");
+});
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}/`);
+    console.log(`server is running on port ${port}.`);
 });
 
 
